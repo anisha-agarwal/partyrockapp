adabeee4e7aed8e4012287e8c8c8029246c6d1b6
Anisha Agarwal <anishaagg@gmail.com>
2016-10-17 00:14:55 -0700

    Video loading

--- PartyRockApp/MainViewController.swift
+++ PartyRockApp/MainViewController.swift
@@ -52,5 +52,19 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return partyRocks.count
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let partyRock = partyRocks[indexPath.row]
+        
+        performSegue(withIdentifier: "VideoViewController", sender: partyRock)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? VideoViewController {
+            if let party = sender as? PartyRock {
+                destination.partyRock = party
+            }
+        }
+    }
 }
 
