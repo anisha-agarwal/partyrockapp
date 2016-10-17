bff82251537bac08b537ded8f019a3a8e0aa26c3
Anisha Agarwal <anishaagg@gmail.com>
2016-10-16 23:10:23 -0700

    UI done

---
+++ PartyRockApp/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  PartyRockApp
+//
+//  Created by Agarwal,Anisha on 10/16/16.
+//  Copyright © 2016 Agarwal,Anisha. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
